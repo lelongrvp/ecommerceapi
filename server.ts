f087d23653da8f2ea74674080cbdbd6502cb7494
hieu.hoangtrung <Hieu.HoangTrung@nttdata.com>
2022-05-04 17:29:26 +0700

    add function to check admin

--- server.ts
+++ server.ts
@@ -11,18 +11,29 @@ app.use(express.json());
 app.use(cors());
 app.use("/api/users", userRoute);
 
-export const authenticateToken = (req, res, next) => {
+/**
+ * Authentication middleware verify jwt
+ */
+app.use((req, res, next) => {
 	const authHeader = req.headers["authorization"];
 	const token = authHeader && authHeader.split(" ")[1];
-	if (token == null) return res.sendStatus(401);
+	if (token == null) return res.status(401).send({message: "Unauthorized"});
 
 	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
 		console.log(err);
-		if (err) return res.sendStatus(403);
+		if (err) return res.status(403).send({message: "Invalid token"});
 		req.user = user;
 		next();
 	});
-};
+});
+
+export const authenticateAdmin = (req, res, next) => {
+	const authHeader = req.headers["authorization"];
+	const token = authHeader && authHeader.split(" ")[1];
+
+	const [isAdmin] = jwt.decode(token).payload;
+	return isAdmin ? next() : res.status(401).send({message: "Unauthorized user"})
+}
 
 app.listen(process.env.SERVER_PORT || 5000, () => {
 	console.log("Backend server is running");
