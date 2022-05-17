7aa78ef2617c2a85552a027c36bb4ee5476af0e8
poochswadge <lelongrvp7@gmail.com>
2022-05-17 13:55:34 +0700

    add cart

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
-
+const cartRoute = require("./routes/cart");
 dotenv.config();
 
 mongoose
@@ -23,6 +23,7 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
 
 app.listen(process.env.PORT || 5000, ()=>{
     console.log("Backend server is running")
