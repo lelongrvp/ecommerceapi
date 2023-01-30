31fde5dfac650378640ec387a6c3535f33b2610e
Le Duc Long <62542108+lelongrvp@users.noreply.github.com>
2023-01-30 12:03:21 +0700

    add doc

--- routes/cart.js
+++ routes/cart.js
@@ -1,67 +1,71 @@
+/* The above code is creating a router for the Cart model. */
+/* This is importing the Cart model, the verifyToken file, and the express router. */
 const Cart = require("../models/Cart");
 const {
-    verifyToken, 
-    verifyTokenAndAuthorization, 
-    verifyTokenAndAdmin
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
 } = require("./verifyToken");
 
 const router = require("express").Router();
 
 //Create Cart
-router.post("/", verifyToken, async (req,res)=>{
-    const newCart = new Cart(req.body);
-    try{
-        const savedCart = await newCart.save();
-        res.status(200).json(savedCart);
-    } catch(err){
-        res.status(500).json(err);
-    }
+/* This is creating a new cart. */
+router.post("/", verifyToken, async (req, res) => {
+  const newCart = new Cart(req.body);
+  try {
+    const savedCart = await newCart.save();
+    res.status(200).json(savedCart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 //Update Cart
-router.put("/:id", verifyTokenAndAuthorization, async (req,res)=>{
-    try{
-        const updatedCart = await Cart.findByIdAndUpdate(
-            req.params.id,
-            {
-                $set: req.body,
-            },
-            {new:true}
-        );
-        res.status(200).json(updatedCart);
-    } catch(err){
-        res.status(500).json(err);
-    }
+/* This is updating the cart. */
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const updatedCart = await Cart.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedCart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 //Delete Cart
-router.delete("/:id", verifyTokenAndAuthorization, async (req,res)=>{
-    try{
-        await Cart.findByIdAndDelete(req.params.id);
-        res.status(200).json("Cart has been deleted...")
-    } catch(err){
-        res.status(500).json(err)
-    }
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await Cart.findByIdAndDelete(req.params.id);
+    res.status(200).json("Cart has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 //Get User's cart
-router.get("/find/:userId", verifyTokenAndAuthorization, async (req,res)=>{
-    try{
-        const cart = await Cart.findOne({userId: req.params.userId})
-        res.status(200).json(cart);
-    } catch(err){
-        res.status(500).json(err);
-    }
+router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const cart = await Cart.findOne({ userId: req.params.userId });
+    res.status(200).json(cart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 //Get all
-router.get("/", verifyTokenAndAdmin, async (req,res)=>{
-    try{
-        const carts = await Cart.find();
-        res.status(200).json(carts);
-    }catch(err){
-        res.status(500).json(err);
-    }
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const carts = await Cart.find();
+    res.status(200).json(carts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,41 +1,48 @@
 const jwt = require("jsonwebtoken");
 
+/**
+ * VerifyTokenAndAuthorization: verifies the token and checks if the user is the owner of the resource
+ * or is an admin
+ * @param req - The request object.
+ * @param res - The response object.
+ * @param next - This is a function that you call when you want to move on to the next middleware.
+ */
 const verifyToken = (req, res, next) => {
-    const authHeader = req.headers.token;
-    if (authHeader) {
-      const token = authHeader.split(" ")[1];
-      jwt.verify(token, process.env.JWT_SEC, (err, user) => {
-        if (err) res.status(403).json("Token is not valid!");
-        req.user = user;
-        next();
-      });
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
+      if (err) res.status(403).json("Token is not valid!");
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.status(401).json("You are not authenticated!");
+  }
+};
+
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
     } else {
-      return res.status(401).json("You are not authenticated!");
+      res.status(403).json("You are not alowed to do that!");
     }
-  };
-  
-  const verifyTokenAndAuthorization = (req, res, next) => {
-    verifyToken(req, res, () => {
-      if (req.user.id === req.params.id || req.user.isAdmin) {
-        next();
-      } else {
-        res.status(403).json("You are not alowed to do that!");
-      }
-    });
-  };
-  
-  const verifyTokenAndAdmin = (req, res, next) => {
-    verifyToken(req, res, () => {
-      if (req.user.isAdmin) {
-        next();
-      } else {
-        res.status(403).json("You are not alowed to do that!");
-      }
-    });
-  };
-  
-  module.exports = {
-    verifyToken,
-    verifyTokenAndAuthorization,
-    verifyTokenAndAdmin,
-  };
+  });
+};
+
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not alowed to do that!");
+    }
+  });
+};
+
+module.exports = {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+};
